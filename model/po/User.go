2303a016a5fcb72608b9c05e0f8257e3fd2cf394
lishanwei <jiuxiajingfan@163.com>
2024-07-02 16:55:38 +0800

    jwt add

--- model/po/User.go
+++ model/po/User.go
@@ -7,21 +7,25 @@ import (
 )
 
 type User struct {
-	Id         int64           `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name       string          `json:"name"`
-	Password   string          `json:"password"`
-	Nickname   string          `json:"nickname"`
+	Id         int64           `gorm:"primaryKey" json:"id"`
+	Name       string          `gorm:"type:varchar(20)" json:"name"`
+	Password   string          `gorm:"type:varchar(100)" json:"password"`
+	Nickname   string          `gorm:"type:varchar(20)" json:"nickname"`
 	GmtCreate  utils.LocalTime `gorm:"autoCreateTime:nano" json:"gmtCreate"`
-	Imgurl     string          `json:"imgUrl"`
-	Github     string          `json:"github"`
-	Email      string          `json:"email"`
-	Record     string          `json:"record"`
-	Title      string          `json:"title"`
-	Title2     string          `json:"title2"`
-	Background string          `json:"background"`
+	Imgurl     string          `gorm:"type:varchar(500)" json:"imgUrl"`
+	Github     string          `gorm:"type:varchar(500)" json:"github"`
+	Email      string          `gorm:"type:varchar(500)" json:"email"`
+	Record     string          `gorm:"type:varchar(100)" json:"record"`
+	Title      string          `gorm:"type:varchar(200)" json:"title"`
+	Title2     string          `gorm:"type:varchar(200)" json:"title2"`
+	Background string          `gorm:"type:text" json:"background"`
 }
 
 func FindUser(dto dto.LoginDTO) (user User) {
 	model.Db.Table("t_user").Where("name = ?", dto.Username).First(&user)
 	return user
+}
+
+func (User) TableName() string {
+	return "t_user"
 }
